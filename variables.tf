37e0ea88d20a786bb6e43c712ba1eb92af08601f
Daniel Allen <dan@aeternagroup.net>
2026-01-11 18:05:47 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "sentinel_data_connector_office_365s" {
+  description = <<EOT
+Map of sentinel_data_connector_office_365s, attributes below
+Required:
+    - log_analytics_workspace_id
+    - name
+Optional:
+    - exchange_enabled
+    - sharepoint_enabled
+    - teams_enabled
+    - tenant_id
+EOT
+
+  type = map(object({
+    log_analytics_workspace_id = string
+    name                       = string
+    exchange_enabled           = optional(bool, true)
+    sharepoint_enabled         = optional(bool, true)
+    teams_enabled              = optional(bool, true)
+    tenant_id                  = optional(string)
+  }))
+}
+
